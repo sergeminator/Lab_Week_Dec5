10fb8f10f8b083b03377a4e8b25359369a4cb635
Sergio <sergemin8or@gmail.com>
2017-12-07 09:00:54 -0500

    Implementing the Thursday function
    Implementing the Thursday function

--- WeekNov4Lab/HelperFunctions.cpp
+++ WeekNov4Lab/HelperFunctions.cpp
@@ -27,7 +27,8 @@ void DisplayIntroMessage(HANDLE hConsole)
 	//BACKGROUND_INTENSITY	Background color is intensified.
 
 	// Combine colors in forground to create cyan
-	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY | BACKGROUND_BLUE);
+	//SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY | BACKGROUND_BLUE);
+	SetConsoleTextAttribute(hConsole, 3);
 
 	SetTextCoordinates(48, 16);
 	std::cout << "***********" << std::endl;
@@ -93,5 +94,42 @@ void DisplayWednesdayLabUserList(HANDLE hConsole)
 
 void DisplayThursdayLabUserList(HANDLE hConsole)
 {
+	system("cls"); // clear screen before we start drawing again
 
+	int row = 5;
+	int col = 10;
+
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+	SetTextCoordinates(col, row);
+	++row;
+
+	std::cout << "------------------------------------------------------------------------" << std::endl;
+
+	// Copy every thing from here
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY | FOREGROUND_INTENSITY);
+	SetTextCoordinates(col, row);
+	++row;
+
+	std::cout << "                 Sergio Juarez           sergeminator                   " << std::endl;
+	// To Here
+
+	/******************************** Paste your new code here *************************************/
+
+
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+	SetTextCoordinates(col, row);
+	++row;
+
+	std::cout << "------------------------------------------------------------------------" << std::endl;
+
+	row += 5;
+	SetConsoleTextAttribute(hConsole, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED);
+	SetTextCoordinates(col, row);
+	++row;
+
+	std::cout << "Enter any text to continue" << std::endl;
+
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+	std::string userInput = "";
+	std::cin >> userInput;
 }
